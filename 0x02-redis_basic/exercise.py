bb7f5a18ac3d079f9add6b28b6c88c28a3e390f4
Edwin Ade <edwinmbonyjr@gmail.com>
2024-10-23 15:20:19 +0100

    feat: task 0 & 1 solution

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+'''
+
+0 - Writing strings to Redis
+
+'''
+import redis
+from typing import Union, Callable
+from uuid import uuid4
+
+
+class Cache:
+    '''Cache class'''
+    def __init__(self) -> None:
+        '''dunder init for Cache class'''
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        '''stores a value in the Redis db using a random uuid4 as key'''
+        data_key = str(uuid4())
+        self._redis.set(name=data_key, value=data)
+        return data_key
+
+    def get(self, key: str, fn: Callable) -> Union[str, bytes, int, float]:
+        '''gets information from redis database and formats it'''
+        if not key:
+            return None
+        data = self._redis.get(key)
+        if fn and data:
+            return fn(data)
+        else:
+            return data
+
+    def get_str(self, key: str) -> str:
+        '''parameterizes get with str method'''
+        if not key:
+            return None
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        '''parameterizes get with int method'''
+        if not key:
+            return None
+        return self.get(key, int)
