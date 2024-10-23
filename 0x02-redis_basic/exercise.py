2745c49977aeb62ecea52554b99a0c5ceecd8512
Edwin Ade <edwinmbonyjr@gmail.com>
2024-10-23 15:43:41 +0100

    fix: add optional argument for get

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -22,18 +22,25 @@ class Cache:
         self._redis.set(name=data_key, value=data)
         return data_key
 
-    def get(self, key: str, fn: Callable=None) -> Union[str, bytes, int, float]:
+    def get(self, key: str,
+            fn: Callable = None) -> Union[str, bytes, int, float]:
         '''gets information from redis database and formats it'''
+        if not key:
+            return None
         data = self._redis.get(key)
-        if fn:
+        if fn and data:
             return fn(data)
         else:
             return data
 
     def get_str(self, key: str) -> str:
         '''parameterizes get with str method'''
+        if not key:
+            return None
         return self.get(key, lambda v: v.decode('utf-8'))
 
     def get_int(self, key: str) -> int:
         '''parameterizes get with int method'''
-        return self.get(key, lambda v: int(v))
+        if not key:
+            return None
+        return self.get(key, int)
